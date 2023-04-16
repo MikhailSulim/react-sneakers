8da8b55559100c4516e68f78725e37971040e04d
Mikhail Sulim <mika-1984@yandex.ru>
2023-04-16 23:42:47 +0300

    feature: внедрить функцию поиска товара

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState("");
 
   React.useEffect(() => {
     fetch("https://643a71f5bd3623f1b9b37efb.mockapi.io/items")
@@ -17,35 +18,61 @@ function App() {
       .then((res) => {
         setItems(res);
       });
-  }, []); 
+  }, []);
 
   const onAddToCart = (obj) => {
     setCartItems([...cartItems, obj]);
-  }
+  };
+
+  const onChangeSearchInput = (e) => {
+    setSearchValue(e.target.value);
+  };
 
   return (
     <div className="wrapper clear">
-      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} />
+      )}
 
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center mb-40 justify-between">
-          <h1>Все кроссовки</h1>
+          <h1>
+            {searchValue ? `Поиск по запросу: ${searchValue}` : "Все кроссовки"}
+          </h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input type="text" placeholder="Поиск..." />
+            {searchValue && (
+              <img
+                className="clear cu-p"
+                src="/img/btn-remove.svg"
+                onClick={() => setSearchValue("")}
+                alt="Clear"
+              />
+            )}
+            <input
+              onChange={onChangeSearchInput}
+              type="text"
+              placeholder="Поиск..."
+              value={searchValue}
+            />
           </div>
         </div>
 
         <div className="d-flex flex-wrap">
-          {items.map((item) => (
-            <Card
-              title={item.title}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onPlus={(obj) => onAddToCart(obj)} 
-            />
-          ))}
+          {items
+            .filter((item) =>
+              item.title.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item, index) => (
+              <Card
+                key={index}
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onPlus={(obj) => onAddToCart(obj)}
+              />
+            ))}
         </div>
       </div>
     </div>
